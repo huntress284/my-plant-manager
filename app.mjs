9015c822aa63a8749d45ad1fbc772b96c9498ac4
Kayla Hunt <khunt6@ycp.edu>
2023-03-11 20:01:40 -0500

    using multer photo upload

--- app.mjs
+++ app.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import {fileURLToPath} from 'url';
 import cors from 'cors';
-import fs from 'fs';
+import multer from 'multer';
 
 const app = express();
 
@@ -37,6 +37,30 @@ app.get('/nursery', async (req, res) => {
     res.status(200).render('nursery');
 });
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, req.body.picname + ".jpg")
+    }
+})
+var upload = multer({ storage: storage })
 
+app.use('/uploads', express.static('uploads'));
+
+app.post('/', upload.single('profile-file', 'picname'), async function (req, res, next) {
+    // req.file is the `profile-file` file
+    // req.body will hold the text fields, if there were any
+    console.log(JSON.stringify(req.body.picname))
+    console.log(JSON.stringify(req.file))
+
+    const url = 'http://localhost:3001/api/plants';
+    const response = await fetch(url);
+    const json = await response.json();
+    res.locals.plants = json;
+    console.log(res.locals);
+    res.status(200).render('home');
+})
 
 export {app}
