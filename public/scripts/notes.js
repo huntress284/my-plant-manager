f2da1f63da8f59d85cdbcff4aae3a5805ae594ae
Kayla Hunt <khunt6@ycp.edu>
2023-04-19 22:46:53 -0400

    cleaning up

---
+++ public/scripts/notes.js
@@ -0,0 +1,26 @@
+async function add_note(x, y) {
+
+    console.log("id of button: " + x)
+    console.log("note for specific: " + y)
+
+    const note = document.querySelector(`[data-noteID= "${x}"]`).value;
+    // // note
+    console.log("should be: " + note);
+
+    const url = 'http://localhost:3001/api/plants/notes';
+    const options = {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            plantId: x,
+            note: note
+        })
+    };
+    const response = await fetch(url, options);
+    if (response.ok) {
+        location.reload();
+    }
+
+}
