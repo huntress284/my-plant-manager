dc596631238c4c3b1434ea7cb060ce1ee439b4a4
Kayla Hunt <khunt6@ycp.edu>
2023-04-24 23:03:11 -0400

    cleaning

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,62 +1,62 @@
-async function get_info(id, x) {
-
-    console.log(id, x);
-
-    // If plant's ID is null (was not created with API ID)
-    if (x === null) {
-
-        document.querySelector(`[plantInformation= "${id}"]`).innerHTML = ' No information from API :( '
-
-    } else {
-        x.toString();
-        console.log("Card ID: " + id);
-
-        var myVar;
-        myVar = setTimeout(showInfo, 1000);
-        document.querySelector(`[loader= "${id}"]`).style.display = "block";
-
-        // PLANT DETAILS
-        const url = "https://perenual.com/api/species/details/" + x + "?key=sk-8k2463e99f604d8a438";
-        const options = {
-            method: 'GET'
-        };
-        const response = await fetch(url, options);
-        if (!response.ok) {
-            console.log(response.status);
-            document.getElementById("overlay").style.display = "block";
-        }
-        const json = await response.json();
-
-
-        // TODO: array of elements
-        const common_name = json.common_name;
-        const scientific_name = json.scientific_name;
-        const propagation = json.propagation;
-        const watering = json.watering;
-        const sunlight = json.sunlight;
-
-        // Clear innerHTML if it is already there
-        document.querySelector(`[commonName= "${id}"]`).innerHTML = '';
-        document.querySelector(`[scientific= "${id}"]`).innerHTML = '';
-        document.querySelector(`[sunlight= "${id}"]`).innerHTML = '';
-        document.querySelector(`[watering= "${id}"]`).innerHTML = '';
-        document.querySelector(`[propagation= "${id}"]`).innerHTML = '';
-        //--------------------------------------------------------------------
-        // Add details and show
-        document.querySelector(`[commonName= "${id}"]`).append(common_name);
-        document.querySelector(`[scientific= "${id}"]`).append(scientific_name);
-        document.querySelector(`[sunlight= "${id}"]`).append(sunlight);
-        document.querySelector(`[watering= "${id}"]`).append(watering);
-        document.querySelector(`[propagation= "${id}"]`).append(propagation);
-
-        document.querySelector(`[info= "${id}"]`).style.display = "block";
-
-        async function showInfo() {
-            document.querySelector(`[loader= "${id}"]`).style.display = "none";
-            document.querySelector(`[info= "${id}"]`).style.display = "block";
-        }
-    }
-}
+// async function get_info(id, x) {
+//
+//     console.log(id, x);
+//
+//     // If plant's ID is null (was not created with API ID)
+//     if (x === null) {
+//
+//         document.querySelector(`[plantInformation= "${id}"]`).innerHTML = ' No information from API :( '
+//
+//     } else {
+//         x.toString();
+//         console.log("Card ID: " + id);
+//
+//         var myVar;
+//         myVar = setTimeout(showInfo, 1000);
+//         document.querySelector(`[loader= "${id}"]`).style.display = "block";
+//
+//         // PLANT DETAILS
+//         const url = "https://perenual.com/api/species/details/" + x + "?key=sk-8k2463e99f604d8a438";
+//         const options = {
+//             method: 'GET'
+//         };
+//         const response = await fetch(url, options);
+//         if (!response.ok) {
+//             console.log(response.status);
+//             document.getElementById("overlay").style.display = "block";
+//         }
+//         const json = await response.json();
+//
+//
+//         // TODO: array of elements
+//         const common_name = json.common_name;
+//         const scientific_name = json.scientific_name;
+//         const propagation = json.propagation;
+//         const watering = json.watering;
+//         const sunlight = json.sunlight;
+//
+//         // Clear innerHTML if it is already there
+//         document.querySelector(`[commonName= "${id}"]`).innerHTML = '';
+//         document.querySelector(`[scientific= "${id}"]`).innerHTML = '';
+//         document.querySelector(`[sunlight= "${id}"]`).innerHTML = '';
+//         document.querySelector(`[watering= "${id}"]`).innerHTML = '';
+//         document.querySelector(`[propagation= "${id}"]`).innerHTML = '';
+//         //--------------------------------------------------------------------
+//         // Add details and show
+//         document.querySelector(`[commonName= "${id}"]`).append(common_name);
+//         document.querySelector(`[scientific= "${id}"]`).append(scientific_name);
+//         document.querySelector(`[sunlight= "${id}"]`).append(sunlight);
+//         document.querySelector(`[watering= "${id}"]`).append(watering);
+//         document.querySelector(`[propagation= "${id}"]`).append(propagation);
+//
+//         document.querySelector(`[info= "${id}"]`).style.display = "block";
+//
+//         async function showInfo() {
+//             document.querySelector(`[loader= "${id}"]`).style.display = "none";
+//             document.querySelector(`[info= "${id}"]`).style.display = "block";
+//         }
+//     }
+// }
 
 
 
